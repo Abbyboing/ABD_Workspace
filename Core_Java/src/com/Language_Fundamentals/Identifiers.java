72d873c54e99355cd89d3bb1d727aa911e5da857
sabdullah_FSS <sabdullah@fss.co.in>
2025-02-08 12:29:08 +0530

    Initial Commit
    Signed-off-by: sabdullah_FSS <sabdullah@fss.co.in>

---
+++ Core_Java/src/com/Language_Fundamentals/Identifiers.java
@@ -0,0 +1,15 @@
+package com.Language_Fundamentals;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+	}
+
+}
+//Identifiers: Class name
+//main: method name
+//String[]: Class name
+//args: variable name
+//x: variable name
---
+++ Core_Java/src/com/Language_Fundamentals/Reserve_Word.java
@@ -0,0 +1,44 @@
+package com.Language_Fundamentals;
+
+public class Reserve_Word {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Allowed characters: A-Za-z0-9$_
+		int Total_Number;
+		int Total#;
+		
+		int Total123;
+		int 123Total;
+		
+		int number;
+		int Number;
+		int NUMBER;
+		
+		int xxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzz = 10;
+		System.out.println(xxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzz);
+		
+		int x;
+		int if;
+		
+		int String = 999;
+		System.out.println(String);
+		
+		int Runnable = 888;
+		System.out.println(Runnable);
+		
+		//Total_number (valid)
+		//Total# (invalid)
+		//123total (invalid)
+		//Total123 (valid)
+		//Ca$h (valid)
+		//_$_$_$_$ (valid)
+		//all@handle (invalid)
+		//java2share (valid)
+		//Integer (valid)
+		//Int (valid)
+		//int (invalid)
+		
+	}
+
+}
